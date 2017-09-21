f0d424796e0b0444151d2940c95ed6baf5c7f920
bp9614 <bpham9614@gmail.com>
2017-09-21 09:59:23 -0400

    Better variable names

--- permutations.py
+++ permutations.py
@@ -1,27 +1,27 @@
 import string
 
 
-def insertion(words) -> set:
-    return {''.join((word[:i], char, word[i:])) for word in words
+def insertion(iterable) -> set:
+    return {''.join((word[:i], char, word[i:])) for word in iterable
             for i in range(len(word)+1) for char in string.ascii_lowercase}
 
 
-def deletion(words):
-    return {''.join((word[:i], word[i+1:])) for word in words
+def deletion(iterable) -> set:
+    return {''.join((word[:i], word[i+1:])) for word in iterable
             if len(word) > 1 for i in range(len(word))}
 
 
-def substitution(words):
-    return {''.join((word[:i], char, word[i+1:])) for word in words
+def substitution(iterable) -> set:
+    return {''.join((word[:i], char, word[i+1:])) for word in iterable
             for i in range(len(word)) for char in string.ascii_lowercase}
 
 
-def transposition(words):
+def transposition(iterable) -> set:
     return {''.join((word[:i], word[i+1], word[i], word[i+2:]))
-            for word in words for i in range(len(word)-1)}
+            for word in iterable for i in range(len(word)-1)}
 
 
-def get_permutations(word, edit_dist):
+def get_permutations(word, edit_dist) -> set:
     perms = {word}
 
     for edit in range(edit_dist):
