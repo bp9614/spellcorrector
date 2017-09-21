b519192e872567932206e112be30def2da86eef6
bp9614 <bpham9614@gmail.com>
2017-09-21 09:18:18 -0400

    Functions -> script

--- spell_correction.py
+++ spell_correction.py
@@ -2,54 +2,44 @@ import permutations
 import re
 from collections import Counter
 
+while True:
+    try:
+        edit_dist = int(input("Enter edit distance (as an integer): "))
+    except ValueError:
+        continue
+    else:
+        break
 
-def suggestions(string, dictionary, edit_dist=1):
-    return [perm for perm in permutations.get_permutations(string, edit_dist)
-            if perm in dictionary]
+with open('sample/dictionary.txt', 'r') as dict_file, \
+        open('sample/test.txt', 'r') as test_file, \
+        open('sample/sample.txt', 'r', encoding='utf8') as sample_file, \
+        open('sample/output.txt', 'w') as output:
+    dictionary = {word.lower() for word
+                  in filter(None, dict_file.read().splitlines())}
+    test = [word.lower() for line in test_file.read().splitlines()
+            for word in filter(None, re.split("[^A-Za-z']", line))]
+    sample = Counter(word.lower() for line in sample_file.read().splitlines()
+                     for word in filter(None, re.split("[^A-Za-z']", line)))
 
+    for item in test:
+        perms = permutations.get_permutations(item, edit_dist)
 
-def best_fit(string, dictionary, sample, edit_dist=1):
-    return max([(perm, sample[perm]) if perm in sample else (perm, 0)
-                for perm in permutations.get_permutations(string, edit_dist)
-                if perm in dictionary], key=lambda x: x[1])[0]
+        perms.update({''.join((perm[:len(perm)-1], "'", perm[len(perm)-1]))
+                      for perm in perms
+                      if perm.endswith('s') and len(perm) > 1})
 
+        words = sorted([(perm, sample[perm]) if perm in sample
+                        else (perm, 0) for perm in perms
+                        if perm in dictionary])
 
-def sample_test(edit_dist=1):
-    with open('sample/dictionary.txt', 'r') as dict_file,\
-            open('sample/test.txt', 'r') as test_file,\
-            open('sample/sample.txt', 'r', encoding='utf8') as sample_file,\
-            open('sample/output.txt', 'w') as output:
-        dictionary = {word.lower() for word
-                      in filter(None, dict_file.read().splitlines())}
-        test = [word.lower() for line in test_file.read().splitlines()
-                for word in filter(None, re.split("[^A-Za-z']", line))]
-        sample = Counter(word.lower() for line in sample_file.read().splitlines()
-                         for word in filter(None, re.split("[^A-Za-z']", line)))
+        if item in dictionary:
+            print(item.capitalize(), "is a real word.", file=output)
 
-        for item in test:
-            perms = permutations.get_permutations(item, edit_dist)
-            include_apostrophes(perms)
-
-            words = sorted([(perm, sample[perm]) if perm in sample
-                            else (perm, 0) for perm in perms
-                            if perm in dictionary])
-
-            if item in dictionary:
-                print(item.capitalize(), "is a real word.", file=output)
-            print('Possible words for ', item, ': ', sep='', end='',
-                  file=output)
-            if words:
-                print(', '.join([w for w,_ in words]), file=output)
-                print('Suggested word:', max(words, key=lambda x: x[1])[0],
-                      end='\n\n', file=output)
-            else:
-                print('No possible words found', end='\n\n', file=output)
-
-
-def include_apostrophes(words):
-    words.update({''.join((word[:len(word)-1], "'", word[len(word)-1]))
-                  for word in words if word.endswith('s') and len(word) > 1})
-
-
-if __name__ == '__main__':
-    sample_test(edit_dist=1)
+        print('Possible words for ', item, ': ', sep='', end='',
+              file=output)
+        if words:
+            print(', '.join([w for w,_ in words]), file=output)
+            print('Suggested word:', max(words, key=lambda x: x[1])[0],
+                  end='\n\n', file=output)
+        else:
+            print('No possible words found', end='\n\n', file=output)
